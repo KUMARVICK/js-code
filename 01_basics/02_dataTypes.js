c2fb009f7d4611dfd01b701f3684a5fd98b710b9
KUMARVICK <125456617+KUMARVICK@users.noreply.github.com>
2024-04-08 12:59:15 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; // trat all js code as newer version
+
+// alert(3+3) we are using nodeJs, not browser
+
+let name = "Vicky" // string
+let age = 27 // number
+let isLoggedIn = false // boolean
+let state;
+
+console.log(typeof "Vicky");
