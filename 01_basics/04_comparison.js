2a974d64b49192c12b4dddde7de3e073e566778d
KUMARVICK <125456617+KUMARVICK@users.noreply.github.com>
2024-04-09 12:45:14 +0000

    Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1); // true
+// console.log(2 >= 1); // true
+// console.log(2 < 1); // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); // true
+
+// always compare same data types
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // true
+
+console.log(undefined == 0); // false
+
+// strict check (===)
+
+console.log("2" === 2); // check strictly datatypes
+
