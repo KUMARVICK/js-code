335f0088b9f59a823fb73d65b61f4c371c59bcc7
KUMARVICK <125456617+KUMARVICK@users.noreply.github.com>
2024-04-09 11:54:31 +0000

    CoversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+// let str3 = str1 = str2
+// console.log(str3)
+
+// console.log("1" +3); //13
+// console.log(1 + 2 + "2") // 32 // javascript always take first datatypes if you give number it will take number and print number
+// console.log("1" + 2 + 3) // 123
+// console.log(3 + 4 * 5 % 5) // 3
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+"") // 0
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter) // 101 (prefix, postfix js mdn site)
